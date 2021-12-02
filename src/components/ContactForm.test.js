f727b9a0fc65619a2ad10d36d8a12383a671a2b8
wb1110 <strengthcoachwb@gmail.com>
2021-12-02 16:02:14 -0600

    one more left

--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -40,18 +40,44 @@ test('renders THREE error messages if user enters no values into any fields.', a
 
 test('renders ONE error message if user enters a valid first name and last name but no email.', async () => {
     render(<ContactForm />);
+    const firstName = screen.queryByPlaceholderText(/edd/i);
+    userEvent.type(firstName, "William");
+    const lastName = screen.queryByPlaceholderText(/burke/i);
+    userEvent.type(lastName, "Buchanan");
+    const submit = screen.queryByText(/submit/i);
+    userEvent.click(submit);
+    const errorEmail = screen.queryByText(/Error: email must be a valid email address./i);
+    expect(errorEmail).toBeInTheDocument();
+
 });
 
 test('renders "email must be a valid email address" if an invalid email is entered', async () => {
     render(<ContactForm />);
+    const email = screen.queryByPlaceholderText(/bluebill1049@hotmail.com/i);
+    userEvent.type(email, "Buchanan");
+    const errorEmail = screen.queryByText(/Error: email must be a valid email address./i);
+    expect(errorEmail).toBeInTheDocument();
 });
 
 test('renders "lastName is a required field" if an last name is not entered and the submit button is clicked', async () => {
     render(<ContactForm />);
+    const submit = screen.queryByText(/submit/i);
+    userEvent.click(submit);
+    const errorLast = screen.queryByText(/Error: lastName is a required field./i);
+    expect(errorLast).toBeInTheDocument();
 });
 
 test('renders all firstName, lastName and email text when submitted. Does NOT render message if message is not submitted.', async () => {
     render(<ContactForm />);
+    const firstName = screen.queryByPlaceholderText(/edd/i);
+    const lastName = screen.queryByPlaceholderText(/edd/i);
+    const email = screen.queryByPlaceholderText(/bluebill1049@hotmail.com/i);
+    const submit = screen.queryByText(/submit/i);
+    const submitted = screen.queryByText(/submitted/i);
+    userEvent.type(firstName, "William");
+    userEvent.type(lastName, "Buchanan");
+    userEvent.type(email, "strengthcoachwb@gmail.com");
+    userEvent.click(submit) ? expect(submitted).toBeInTheDocument() : null;
 });
 
 test('renders all fields text when all fields are submitted.', async () => {
